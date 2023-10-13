8951f43042ec0f81fe4ead6ec84b4decf6ea1c34
philip dutra <monsterphilip10@gmail.com>
2023-10-13 10:54:27 -0400

    completed lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,22 @@
 import requests
 import json
 
+
 class GetRequester:
 
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response_body  = requests.get(self.url)
+        
+        return response_body.content
 
     def load_json(self):
-        pass
+        response_list = []
+        items = json.loads(self.get_response_body())
+        for item in items:
+            response_list.append(item)
+        
+        return response_list
+
